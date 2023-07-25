2cbb18c0d2ddb30ef86fc1380a08ca56edd744e9
Nithin <nithin@askerbot.com>
2023-07-25 14:35:46 +0530

    Fix Inline css

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 const TeamJoin = () => {
-  return (
-    <div className="flex justify-center items-center h-screen flex-col">
-      <h1 className='mb-2' style={{color:'#feefff', fontSize:'36px', fontWeight:700}}>JOIN THE GAME</h1>
-      <Input className='bg-input-box w-1/2 h-14 text-center ' placeholder='Enter the code...' style={{color:'#feefff', fontSize:'28px', textTransform:'uppercase'}} autoCapitalize="characters"/>
-      <Button className='my-4 w-1/12 bg-green-button hover:bg-sky-700'>JOIN</Button>
-    </div>
-  )
-}
+	return (
+		<div className="flex justify-center items-center h-screen flex-col">
+			<h1 className="mb-3 font-bold text-4xl">JOIN THE GAME</h1>
+			<Input className="bg-gray-700 text-2xl  w-1/2 h-14 text-center uppercase" placeholder="Enter the code..." />
+			<Button className="bg-green-500 my-4 w-1/1 hover:bg-blue-400">JOIN</Button>
+		</div>
+	);
+};
 
-export default TeamJoin
+export default TeamJoin;
