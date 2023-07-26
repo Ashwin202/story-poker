fb4a96a1a2fba7669929952f58a318cd39917d60
gopsdv42 <gopi@codty.com>
2023-07-26 15:11:06 +0530

    Add BottomBar

--- src/app/polling/page.tsx
+++ src/app/polling/page.tsx
@@ -1,12 +1,12 @@
-
+import BottomBar from "@/components/BottomBar";
 
 const page = () => {
     return (
             <div>
-                <div className="absolute inset-x-0 top-0 h-16 ...">Top</div>
+                <div className="absolute inset-x-0 top-0 h-24 ...">Top</div>
                 <div className="absolute inset-y-0 right-0 w-16 ...">Right</div>
-                <div className="absolute inset-x-0 bottom-0 h-16 ...">
-                    Bottom
+                <div className="absolute inset-x-0 bottom-0 h-28 ...">
+                    <BottomBar/>
                 </div>
             </div>
     );
